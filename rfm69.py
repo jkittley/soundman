57d7da3b8e175baf81164e28d837d5f3d585619b
Jacob Kittley-Davies <jacob@kittley.com>
2018-04-19 14:54:56 +0100

    Auto create channels and sensors

--- rfm69.py
+++ rfm69.py
@@ -4,23 +4,64 @@ from datetime import datetime
 from RFM69 import Radio, FREQ_433MHZ
 import requests
 
-sd_store_url = 'http://raspberrypi.local/'
+
 
 # login
-login_url  = sd_store_url + 'admin/login/'
+base_url = 'http://raspberrypi.local/'
+login_url  = base_url + 'admin/login/'
+sd_store_url = base_url + 'sdstore/'
 session    = requests.Session()
 login_page = session.get(login_url)
 session.post(login_url, data=dict(username="sensor", password="sdstoredevice", csrfmiddlewaretoken=login_page.cookies['csrftoken']))
 
+freqs = {
+    "volume": 1,
+    "battery": 1,
+    "RSSI": 1,
+}
+
+units = {
+    "volume": "db",
+    "battery": "v",
+    "RSSI": 'db',
+}
+
+def create_sensor(sensor_id):
+    # print("Creating sensor")
+    r = session.post(
+        sd_store_url + 'sensors/', 
+        dict(mac=sensor_id, name=sensor_id, sensor_type="sound-node")
+    )
+    return r.status_code == 200 or r.status_code == 201
+
+def create_channel(sensor_id, channel_name):
+    # print("Creating channel")
+    r = session.post(
+        sd_store_url + 'sensor/{}/{}/'.format(sensor_id, channel_name), 
+        {"reading_frequency": freqs[channel_name], "unit": units[channel_name] }
+    )
+    return r.status_code == 200
+
+def save_reading(sensor_id, channel_id, ts, value):
+    # print("Creating reading")
+    r = session.post(
+        sd_store_url + 'sensor/{}/{}/data/'.format(sensor_id, channel_id), 
+        { "data": json.dumps([dict(timestamp=ts, value=value)]) }
+    )
+    # print(r.text)
+    return r.status_code == 200
+
 def process_packet(packet):
     ts = packet.received.strftime('%a %b %d %H:%M:%S %Y')
-    for key, value in dict(volume=packet.data[0], battery=packet.data[1], RSSI=packet.RSSI).items(): 
+    sensor_id = "sensor{}".format(packet.sender)
 
-        post_data = { "data":  json.dumps([dict(timestamp=ts, value=value)]) }
-        data_url  = sd_store_url + 'sdstore/sensor/sensor_{}/{}/data/'.format(packet.sender, key)
-
-        r = session.post(data_url, post_data)
-        print(r.text)
+    for key, value in dict(volume=packet.data[0], battery=packet.data[1], RSSI=packet.RSSI).items():         
+        
+        if not save_reading(sensor_id, key, ts, value):
+            if create_sensor(sensor_id):
+                create_channel(sensor_id, key)
+            else:
+                print ("Failed to create sensor")
 
 
 print ("Starting")
