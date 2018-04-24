1c1d9e4ba7aebe50a26cd16fef69a9557ebf2303
Jacob Kittley-Davies <jacob@kittley.com>
2018-04-24 08:39:38 +0100

    UI Tweeks

--- frontend/views.py
+++ frontend/views.py
@@ -22,7 +22,8 @@ def index(request):
         # .filter(timestamp__gte=form.cleaned_data['start'])\
         # .filter(timestamp__lte=form.cleaned_data['end'])\
    
-    for r in readings[:25]:
+
+    for r in readings[:30]:
         key = datetime.strftime(r.timestamp, "%Y-%m-%d %H:%M:%S")
         if key not in by_time:
             by_time[key] = {}
