65181cae643ef8c82c0a42bbd07aa0578e0c597b
Jacob Kittley-Davies <jacob@kittley.com>
2018-06-29 14:02:29 +0100

    Fixed direct data save issues

--- rfmSerial.py
+++ rfmSerial.py
@@ -1,19 +1,17 @@
-import time
+import time, sys, glob, os
 import json
 from datetime import datetime
-import requests
 import serial
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soundman.settings")
+import django
+django.setup()
+from sd_store.models import Channel, SensorReading, Sensor
+from django.utils import timezone
+from django.contrib.auth.models import User
+from soundman.settings import SDSTORE_USER, SDSTORE_PASS
+
 # login
-base_url = 'http://raspberrypi.local/'
-login_url  = base_url + 'admin/login/'
-sd_store_url = base_url + 'sdstore/'
-session    = requests.Session()
-login_page = session.get(login_url)
-session.post(login_url, data=dict(username="sensor", password="sdstoredevice", csrfmiddlewaretoken=login_page.cookies['csrftoken']))
-
-SERIAL_PORT = '/dev/ttyACM0'
-
 freqs = {
     "volume": 1,
     "battery": 1,
@@ -26,59 +24,101 @@ units = {
     "RSSI": 'db',
 }
 
-def create_sensor(sensor_id):
-    # print("Creating sensor")
-    r = session.post(
-        sd_store_url + 'sensors/', 
-        dict(mac=sensor_id, name=sensor_id, sensor_type="sound-node")
-    )
-    return r.status_code == 200 or r.status_code == 201
-
-def create_channel(sensor_id, channel_name):
-    # print("Creating channel")
-    r = session.post(
-        sd_store_url + 'sensor/{}/{}/'.format(sensor_id, channel_name), 
-        {"reading_frequency": freqs[channel_name], "unit": units[channel_name] }
-    )
-    return r.status_code == 200
 
 def save_reading(sensor_id, channel_id, ts, value):
-    # print("Creating reading")
-    r = session.post(
-        sd_store_url + 'sensor/{}/{}/data/'.format(sensor_id, channel_id), 
-        { "data": json.dumps([dict(timestamp=ts, value=value)]) }
-    )
-    # print(r.text)
-    return r.status_code == 200
+    print("Creating reading for ", channel_id)
+   
+    u, created = User.objects.get_or_create(username=SDSTORE_USER)
+    if created:
+        u.set_password(SDSTORE_PASS)
+
+    s, _ = Sensor.objects.get_or_create(mac=sensor_id, user=u)
+    c, _ = Channel.objects.get_or_create(name=channel_id, reading_frequency=1)
+    r = SensorReading(timestamp=ts, sensor=s, channel=c, value=value)
+    r.save()
+    return True
 
 def process_line(line):
     if line.startswith("{"):
-        print(line)
-        # try:
-        data = json.loads(line)
-        print(data)
-        
-        ts = datetime.utcnow().strftime('%a %b %d %H:%M:%S %Y')
+        try:
+            data = json.loads(line)
+            print(data)       
+            ts = timezone.now()
 
-        sensor_id = "sensor{}".format(data['sender'])
-        for key, value in dict(volume=data['pay_volume'], battery=data['pay_battery'], RSSI=data['rssi']).items():      
-            if not save_reading(sensor_id, key, ts, value):
-                if create_sensor(sensor_id):
-                    create_channel(sensor_id, key)
-                else:
-                    print ("Failed to create sensor")
-            else:
-                print ("Saved")
-        # except:
-        #     pass
+            sensor_id = "sensor{}".format(data['sender'])
+            for key, value in dict(volume=data['pay_volume'], battery=data['pay_battery'], RSSI=data['rssi']).items():      
+                save_reading(sensor_id, key, ts, value)
+
+        except Exception as e:
+            print ("Error", e)
 
 
-print ("Starting Serial Monitor")
-with serial.Serial(SERIAL_PORT, 115200, timeout=10) as ser:
-    print ("Starting loop")
-    while True:
-        line = ser.readline().decode("utf-8").strip()
-        process_line(line)
-        # transmit(ser, verbose)
-        if not ser.is_open:
-            ser.open()
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+# 
+# Main function 
+# 
+def run():
+    print ("Searching for serial port")
+    ports = serial_ports()
+    for i, p in enumerate(ports): 
+        print ("Testing port {} of {}: {}".format(i, len(ports), p))
+        if ports[i] is None:
+            print("- Port is None")
+            continue
+
+        print ("- Connecting...")
+        with serial.Serial(ports[i], 115200, timeout=10) as ser:
+            
+            attempt = 1
+            max_attempts = 4
+
+            while not ser.is_open:
+                print ("Retry attempt {} of {}".format(attempt, max_attempts))
+                ser.open()
+                attempt += 1
+                time.sleep(2)
+                if attempt > max_attempts:
+                    break
+            
+            if ser.is_open:
+                print("Connected")
+
+            while ser.is_open:
+                line = ser.readline().decode("utf-8").strip()
+                print(line)
+                process_line(line)
+            
+                
+                    
+
+        print ("- Connection failed")
+
+run()
